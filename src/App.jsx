045d9c1bce858e40b19254ffed8ada433d9294e9
nihitha14 <2400032980@kluniversity.in>
2025-10-15 17:30:07 +0530

    completed

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import FormValidation from './FormValidation'
+
+function App() {
+  return (
+    <div>
+      <h2>React Form Validation</h2>
+      <FormValidation />
+    </div>
+  )
+}
+
+export default App
